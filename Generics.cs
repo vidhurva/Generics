234df0b3a5d64643e7f53c4e26466ac67de30728
Vidhur <vidhurvashisht@gmail.com>
2018-05-25 11:08:03 -0400

    Add files via upload

---
+++ Generics.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstConsoleProject
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine(Utility.CompareValues(10,3));
+            Console.WriteLine(Utility.CompareTypes("hello", 54));
+            Console.WriteLine(Utility.CompareTypes("hi", "hello"));
+        }
+    }
+
+    class Utility
+    {
+        public static bool CompareValues<T01, T02> (T01 value01, T02 value02)
+        {
+            return value01.Equals(value02);
+        }
+        public static bool CompareTypes<T01, T02>(T01 type01, T02 type02)
+        {
+            return typeof(T01).Equals(typeof(T02));
+        }
+    }
+
+}
